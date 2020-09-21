58d3e202e7c9d371395c54423e1a534bafd2761c
melonybox <grandmadcow@gmail.com>
2020-09-21 17:06:54 -0400

    day 1 edabit

---
+++ code_challenges/days_1-7/day_1_edabit.cs
@@ -0,0 +1,47 @@
+public class Program
+{
+    public static bool ReturnTrue()
+    {
+      return true;
+    }
+}
+
+public class Program
+{
+	public static int triArea(int b, int h)
+	{
+		return (b*h)/2;
+	}
+}
+
+public class Program
+{
+    public static bool Reverse(bool boolean)
+    {
+      return !boolean;
+    }
+}
+
+public class Program
+{
+    public static string HelloName(string name)
+    {
+      return string.Format("Hello {0}!", name);
+    }
+}
+
+public class Program
+{
+    public static int Sum(int a, int b)
+    {
+      return a+b;
+    }
+}
+
+public class Program
+{
+    public static int Frames(int minutes, int fps)
+		{
+			return (minutes*60)*fps;
+		}
+}
